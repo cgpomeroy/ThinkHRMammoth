293290d57f8ff8a396a94bf9c3d13dca37049757
Colton Pomeroy <me@coltonpomeroy.com>
2020-03-22 20:19:34 -0700

    Updates

---
+++ precache-manifest.d2021019f80a79fbe330c46de2242f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f5f8ddc818e79017855f0926b90725f",
+    "url": "/ThinkHRMammoth/index.html"
+  },
+  {
+    "revision": "77769e2b523b0b8c2c51",
+    "url": "/ThinkHRMammoth/static/css/main.87c2b7fe.chunk.css"
+  },
+  {
+    "revision": "1fad0c240809ffeb971b",
+    "url": "/ThinkHRMammoth/static/js/2.2a1dbb6a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ThinkHRMammoth/static/js/2.2a1dbb6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77769e2b523b0b8c2c51",
+    "url": "/ThinkHRMammoth/static/js/main.8b2de9b9.chunk.js"
+  },
+  {
+    "revision": "6965715e92ba05392bf7",
+    "url": "/ThinkHRMammoth/static/js/runtime-main.56b4d62c.js"
+  }
+]);
